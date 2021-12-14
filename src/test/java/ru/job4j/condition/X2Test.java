4bd9f62ca66b2b43fcd42ae114aa2ebe77acb9a8
Tsypa90 <URSA2004@mail.ru>
2021-12-14 23:16:45 +0300

    1.2. Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class X2Test {
+
+    @Test
+
+    public void result() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+}
