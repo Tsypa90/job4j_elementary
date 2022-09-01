544ac8493504b63bb122a52a47da5270cb43dcf4
Tsypa90 <URSA2004@mail.ru>
2022-09-01 08:33:15 +0300

    6.9. String - это массив [#504884 #337875]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -20,10 +20,10 @@ public class JavaNameValidator {
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return code >= 65 || code <= 95;
+        return code >= 65 && code <= 95;
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return code <= 122 || code >= 97;
+        return code <= 122 && code >= 97;
     }
 }
